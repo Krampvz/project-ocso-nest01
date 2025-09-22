8bfe4b7df8b6f01753246f92ab9b7cd672480eaf
Krampvz <adrianorte17@gmail.com>
2025-09-21 22:45:43 -0600

    Initial config for locations route

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,1 +1,15 @@
-export class CreateLocationDto {}
+import { ArrayNotEmpty, IsArray, IsString, MaxLength } from "class-validator";
+
+export class CreateLocationDto {
+  @IsString()
+  @MaxLength(35)
+  locationName: string;
+
+  @IsString()
+  @MaxLength(160)
+  locationAdress: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  locationLating: number[];
+}
--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,1 +1,16 @@
-export class Location {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Location {
+  @PrimaryGeneratedColumn('increment')
+  locationId: number;
+
+  @Column('text')
+  locationName: string;
+
+  @Column('text')
+  locationAdress: string;
+
+  @Column('simple-array') // ← Cambié de 'array' a 'simple-array'
+  locationLating: number[];
+}
