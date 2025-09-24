27ce4bf2d5e22b436f5cfe37dd75c324a0e6c6cd
Krampvz <adrianorte17@gmail.com>
2025-09-24 14:17:10 -0600

    Auth decorator implemented

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -3,9 +3,13 @@ import { ProvidersService } from './providers.service';
 import { ProvidersController } from './providers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Provider } from './entities/provider.entity';
+import { AuthModule } from '../auth/auth.module'; // ← Esta importación sí está
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Provider])],
+  imports: [
+    TypeOrmModule.forFeature([Provider]),
+    AuthModule, // ← PERO FALTA AGREGARLA AL ARRAY
+  ],
   controllers: [ProvidersController],
   providers: [ProvidersService],
 })
